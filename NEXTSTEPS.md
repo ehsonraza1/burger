d0b088b015678c197993667d7bc1f40bf32c1599
Ehson Raza <ehson.raza1@outlook.com>
2020-07-15 23:10:22 -0500

    Adding final touches

---
+++ NEXTSTEPS.md
@@ -0,0 +1,16 @@
+### Next Steps
+
+TODO
+
+Make a function in server.js that does the following:
+Make api call to /all to get an updated list of burgers
+Empty out the two columns that have burgers
+Populate those two columns with the updated list and devoured burgers
+
+Mps over all the burgers, if devoured write here else there
+Function that can take a single burger object and output some html that displays it in a panel or card or <li>
+Also need to make a button on the undevoured column that lets you devour it
+That button needs to be able to track in the id of the burger
+The id will let you make an api request to that burger's id, and devour it
+
+Once you've devoured OR created a new burger, just call this populate function to refresh you page content
--- server.js
+++ server.js
@@ -27,18 +27,3 @@ app.listen(PORT, function () {
   // Log (server-side) when our server has started
   console.log("Server listening on: http://localhost:" + PORT);
 });
-
-//Todo
-
-// make a function does alot:
-// make api call to /all to get an updated list of burgers
-// empty out the two columns that have burgers
-// populate those two columns with the updated list and devoured-ness
-
-// mape over all the burgers, if devoured write here else there
-// function that can take a single burger object and spit out some html that displays it in a panel or card or <li>
-// also need to make a button on the undevoured column that lets you devour it
-// that button needs to be able to track in the id of the burger
-// the id will let you make an api request to that burger's id, and devour it
-
-// once you've devoured OR created a new burger, just call this populate function to refresh you page content
